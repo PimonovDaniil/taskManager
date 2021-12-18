8c99fb3a727e9eb323db0bb203b402d4919411b7
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2021-12-18 22:41:10 +0700

    Фильтры и даты
    Добавил в задание фильтры и даты(крайнего срока и сдачи задания) если они есть.

--- components/Task.js
+++ components/Task.js
@@ -20,19 +20,41 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const Task: () => Node = ({el}) => {
-
   return (
     <SafeAreaView>
       <View style={styles.taskStyle}>
-        <View style={[{flex: 1}, {backgroundColor: 'red'}, {padding: 10}]}>
-          <Text>Готово!</Text>
+        <View style={styles.TaskIsCompletingStyle}>
+          <Text style={[{color: 'white'}]}>Готово!</Text>
         </View>
-        <View style={[{flex: 5}, {backgroundColor: 'green'},{padding:5}]}>
-          <Text>{el.nameTask}</Text>
-          <Text style={[{marginTop:10}]}>{el.descriptionTask}</Text>
+        <View style={styles.TaskInformationStyle}>
+          <View
+            style={[
+              {display: 'flex'},
+              {flexDirection: 'row'},
+              {alignItems: 'flex-start'},
+            ]}>
+            <View style={[{flex: 1}]}>
+              <Text style={[{color: 'white'}, {fontSize: 20}]}>
+                {el.nameTask}
+              </Text>
+            </View>
+            <View style={styles.FilterStyle}>
+              <Text style={[{color: 'white'}]}>{el.filter}</Text>
+            </View>
+          </View>
+          <View style={[{marginTop: 3}]}>
+            <Text style={[{color: 'white'}]}>{el.descriptionTask}</Text>
+          </View>
+          <View>
+            {el?.deadline !== undefined && (
+              <Text style={[{color: 'white'}, {textAlign: 'right'}]}>Сделать до {el?.deadline}</Text>
+            )}
+            {el?.finishDate !== undefined && (
+              <Text style={[{color: 'white'}, {textAlign: 'right'}]}>Сделано {el?.finishDate}</Text>
+            )}
+          </View>
         </View>
       </View>
-      {/*<Text>{el.nameTask}</Text>*/}
     </SafeAreaView>
   );
 };
@@ -43,11 +65,27 @@ const styles = StyleSheet.create({
     flex: 1,
     borderWidth: 3,
     borderRadius: 2,
-    border: '1px dashed black',
+    border: '1px black',
     backgroundColor: '#8652f7',
     marginTop: 20,
     marginLeft: 30,
     marginRight: 30,
+  },
+  TaskIsCompletingStyle: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 10,
+  },
+  TaskInformationStyle: {
+    flex: 5,
+    backgroundColor: 'green',
+    padding: 5,
+  },
+  FilterStyle: {
+    padding: 2,
+    borderWidth: 3,
+    borderStyle: 'dashed',
+    borderRadius: 5,
   },
 });
 
